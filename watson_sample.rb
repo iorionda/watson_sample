612bfd77fa99b6628d6187da36962e2201f5715c
Iori ONDA <iori.onda@gmail.com>
2013-11-29 17:22:34 +0900

    Initial

---
+++ watson_sample.rb
@@ -0,0 +1,18 @@
+class WatsonSample
+  # [TODO] - method 名を変更
+  def self.helloworld
+    puts 'hello world'
+  end
+end
+
+# [FIXME] - メッセージを変更
+puts "Hello watson"
+
+# [XXXX] - 困っている
+if true
+  puts "true"
+else
+  puts "false"
+end
+
+WatsonSample.helloworld
